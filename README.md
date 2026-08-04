ed522ef23022d55b2473b4a56d07095bb9e2f6df
Jonatan Männchen <jonatan@maennchen.ch>
2026-08-04 15:50:45 +0200

    Release v0.5.1

--- README.md
+++ README.md
@@ -85,7 +85,7 @@ in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:oidcc_plug, "~> 0.5.0"}
+    {:oidcc_plug, "~> 0.5.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Oidcc.Plug.MixProject do
   def project do
     [
       app: :oidcc_plug,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
